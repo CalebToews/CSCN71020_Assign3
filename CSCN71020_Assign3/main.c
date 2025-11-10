5a79a6b7e2b749508d0ad15fd7b531cb57b6a00c
Caleb Toews <Ctoews3893@conestogac.on.ca>
2025-11-09 20:51:44 -0500

    Player2 Invalid Input Test pass

--- CSCN71020_Assign3/main.c
+++ CSCN71020_Assign3/main.c
@@ -13,6 +13,8 @@ char* winnerCheck(char player1[], char player2[]) {
 
 	if (!(strcmp(player1, "rock") == 0 || strcmp(player1, "scissor") == 0 || strcmp(player1, "paper") == 0)) {
 		return "Invalid";
+	} else if (!(strcmp(player2, "rock") == 0 || strcmp(player2, "scissor") == 0 || strcmp(player2, "paper") == 0)) {
+		return "Invalid";
 	}
 
 	if (strcmp(player1, player2) == 0) {
