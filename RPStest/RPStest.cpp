6a4249084a2490a8cbd52d3497aad457d6f7a92d
Caleb Toews <Ctoews3893@conestogac.on.ca>
2025-11-09 19:52:36 -0500

    Tie test made

--- RPStest/RPStest.cpp
+++ RPStest/RPStest.cpp
@@ -11,8 +11,15 @@ namespace RPStest
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(Tie) 
 		{
+			//player 1 and 2 have same input
+			char player1[] = { "rock" };
+			char player2[] = { "rock" };
+
+			char* result = winnerCheck(player1, player2);
+
+			Assert::AreEqual("Draw", result);
 		}
 	};
 }
