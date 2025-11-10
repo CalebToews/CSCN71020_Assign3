07cff1e0b8e530684be0c126522000475e9cde8a
Caleb Toews <Ctoews3893@conestogac.on.ca>
2025-11-09 21:28:57 -0500

    Player2 Win with Paper Test pass

--- CSCN71020_Assign3/main.c
+++ CSCN71020_Assign3/main.c
@@ -24,6 +24,8 @@ char* winnerCheck(char player1[], char player2[]) {
 	if (strcmp(player1, "rock") == 0) {
 		if (strcmp(player2, "scissor") == 0) {
 			return "Player1";
+		} else if (strcmp(player2, "paper") == 0) {
+			return "Player2";
 		}
 	} else if (strcmp(player1, "paper") == 0) {
 		if (strcmp(player2, "rock") == 0) {
